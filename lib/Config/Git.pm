fbd01f19c92f3e4655e371175dbe90f88cb950bd
Kazuhiro Osawa <yappo@shibuya.pl>
2011-02-08 15:16:34 +0900

    initial commit

---
+++ lib/Config/Git.pm
@@ -0,0 +1,32 @@
+package Config::Git;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Config::Git -
+
+=head1 SYNOPSIS
+
+    use Config::Git;
+
+=head1 DESCRIPTION
+
+Config::Git is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
