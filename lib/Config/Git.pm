0c34bdb4c4c453b942706305a745e97bfd58d4fc
Kazuhiro Osawa <yappo@shibuya.pl>
2011-02-08 15:40:27 +0900

    added document

--- lib/Config/Git.pm
+++ lib/Config/Git.pm
@@ -8,21 +8,77 @@ __END__
 
 =head1 NAME
 
-Config::Git -
+Config::Git - read gitconfig files
 
 =head1 SYNOPSIS
 
+in $HOME/.gitconfig
+
+    [foo]
+        a = true
+        k = false
+        b = 
+    [github]
+        user = yappo
+
+in .git/config
+
+    [foo]
+        48 = true;
+    [remote "origin"]
+        url = git@github.com:yappo/p5-Config-Git.Git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+
+in your code
+
     use Config::Git;
+
+    my $conf = Config::Git->load; # load $HOME/.gitconfig and .git/config
+    my $conf = Config::Git->load('.myconfig'); # load .myconfig
+    my $conf = Config::Git->load_with('.myconfig'); # load $HOME/.gitconfig, .git/config and .myconfig
+
+    $conf->get('github.user'); # yappo
+    $conf->get_global('github.user'); # yappo
+
+    $conf->get('remote.origin.url'); # git@github.com:yappo/p5-Config-Git.git
+    $conf->get_global('remote.origin.url'); # undef
+
+    $conf->is_true('foo.a'); # 1
+    $conf->is_true('foo.k'); # 0
+    $conf->is_true('foo.b'); # 0
+    $conf->is_true('foo.48'); # 1
+
+    $conf->is_true_global('foo.a'); # 1
+    $conf->is_true_global('foo.k'); # 0
+    $conf->is_true_global('foo.b'); # 0
+    $conf->is_true_global('foo.48'); # 0
 
 =head1 DESCRIPTION
 
-Config::Git is
+Config::Git is git config file reader. It has not git config file editor.
+
+=head1 METHODS
+
+=head2 load()
+=head2 load(@files)
+
+=head2 load_with(@files)
+
+=head2 get($key)
+=head2 get_global($key)
+
+=head2 is_true($key)
+=head2 is_true_global($key)
+
+=head2 to_hash()
 
 =head1 AUTHOR
 
 Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
 
 =head1 SEE ALSO
+
+L<Config::GitLike>, L<http://www.kernel.org/pub/software/scm/git/docs/git-config.html>
 
 =head1 LICENSE
 
